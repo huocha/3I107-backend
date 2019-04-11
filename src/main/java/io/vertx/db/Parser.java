8ea4fe03d4775be639c65df5c78fbe60d54cf443
Jasmine Ng <dnguyen@ubiqus.com>
2019-04-11 11:36:07 +0200

    rename var

--- src/main/java/io/vertx/db/Parser.java
+++ src/main/java/io/vertx/db/Parser.java
@@ -22,6 +22,7 @@ public class Parser {
     private static String workingDirectory = System.getProperty("user.dir");
 	private static String dataFileName = "/src/ressource/rawTest.csv"; //file source
 	private static String dataFileSearch = "/src/ressource/rawTest.txt"; //new file to be searched data
+	
 	public static String getDataFileSearch() {
 		return dataFileSearch;
 	}
@@ -62,40 +63,41 @@ public class Parser {
         Scanner scanner = new Scanner(file);
         
         String[] firstLine = scanner.nextLine().split(DEFAULT_SEPARATOR); // firstLine is name of columns 
+        
         scanner.nextLine();//pass the line with ","
         
-        IndexColumn ic1=new IndexColumn(0); // IndexColumn by the first column->numero 0 , ie: vendor_name 
-        Index i1=new Index();
-        i1.insert(ic1); 
+        IndexColumn firstColumn = new IndexColumn(0); // IndexColumn by the first column->numero 0 , ie: vendor_name 
+        Index indexFirstColumn = new Index();
+        indexFirstColumn.insert(firstColumn); 
        
-       
-      
-	
-        File ff=new File(workingDirectory+dataFileSearch);
-        boolean fileExiste=ff.exists(); //check if the file research existe?
+        File fileSearch = new File(workingDirectory+dataFileSearch);
+        boolean fileIsExisted = fileSearch.exists(); //check if the file research existe?
         PrintWriter  pw=null;
-        if (!fileExiste) {
-        	
-        	ff.createNewFile();
+        
+        if (!fileIsExisted) {
+        	fileSearch.createNewFile();
         	pw=new PrintWriter(workingDirectory+dataFileSearch);
         }
         try {
         	while (scanner.hasNext()) {
         		String thisLine=scanner.nextLine();
-        		if (!fileExiste) pw.println(thisLine); //write new file
-               
-        		parseLine(thisLine,i1.getIndexCol().get(0));
         		
+        		if (!fileIsExisted) pw.println(thisLine); //write new file
                
+        		parseLine(thisLine,indexFirstColumn.getIndexCol().get(0));
+        		
             }
         }
-      
         catch(Exception e){
         	Console.error(e);
         }
-        if (!fileExiste) pw.close();
+        finally{
+        	pw.close();
+        }
         
-        table.insert(i1); // add index which contain indexColumn of vendor_name to table for ex
+        //if (!fileIsExisted) pw.close();
+        
+        table.insert(indexFirstColumn); // add index which contain indexColumn of vendor_name to table for ex
         
         
         Console.log("-----------");
@@ -103,7 +105,7 @@ public class Parser {
         Console.log("Index: "+table.getIndexes().size());
         //Console.log("Count: "+table.count());
         
-        List<Index> indexx = table.getIndexes();
+        // List<Index> indexx = table.getIndexes();
         
         //Console.log("Index Col: "+ indexx.get(0).getIndexCol().get(0).get("DDS").size() );
 
